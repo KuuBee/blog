773104ccd4b1abc49b1468983c175e6adfaa6d34
yzj <2538340934@qq.com>
2021-03-19 11:06:34 +0800

    更新README

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ npm run build
 echo 打包完成
 echo 开始部署
 echo 开始连接服务器。。。
-ssh aili -t << EOF
+ssh aili -t <<EOF
 echo 连接成功
 cd /home/fontend/
 ls
@@ -13,5 +13,6 @@ exit
 EOF
 echo 删除成功
 echo 开始上传文件
+# 我这里用了scp上传文件 你可以根据你的需求修改
 scp -r /Users/kuubee/Desktop/self_porject/fontend/ng-blog/dist/blog aili:/home/fontend
 echo 部署完成
--- src/app/shared/services/user-info.service.ts
+++ src/app/shared/services/user-info.service.ts
@@ -42,12 +42,3 @@ export class UserInfoService {
     return this._userInfo.name;
   }
 }
-// FIXME 文章详情会自动跳一下记得看一下
-// TODO 最近文章模块数据还没搞
-// TODO 页面缓存也还没搞
-// FIXME 部分按钮和链接 鼠标还会变会手指的状态
-// FIXME 分类和标签页pc上图片太大了 要该小点，移动端大小正合适
-// TODO 关于页还没写 最好今天晚上搞一下
-// TODO 准备第一篇文章
-// TODO 这条不一定要搞 fav模块的图片都是在外网上 考虑要不要扣到本地
-// TODO 鼠标圈圈在鼠标不在视窗内的情况下 最好隐藏 需要监听一下事件
