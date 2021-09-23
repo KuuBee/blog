12a84c2285d1ed7e38560597d9b54fbf9761cb7e
yzj <2538340934@qq.com>
2021-09-23 17:21:55 +0800

    更新小人

--- src/app/shared/components/app-blhx-wives/app-blhx-wives.component.ts
+++ src/app/shared/components/app-blhx-wives/app-blhx-wives.component.ts
@@ -1,5 +1,6 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 interface BlhxData {
   imageList: {
@@ -9,31 +10,72 @@ interface BlhxData {
   lines: {
     lines: string;
     recording: string | null;
-  };
+  }[];
 }
 
 @Component({
   selector: 'app-blhx-wives',
   templateUrl: './app-blhx-wives.component.html',
   styleUrls: ['./app-blhx-wives.component.scss'],
+  animations: [
+    trigger('myInsertRemoveTrigger', [
+      transition(':enter', [
+        style({
+          transform: 'scale(0)',
+          opacity: 0,
+        }),
+        animate(
+          '370ms',
+          style({
+            transform: 'scale(1)',
+            opacity: 1,
+          })
+        ),
+      ]),
+      transition(':leave', [
+        animate(
+          '10000ms',
+          style({
+            transform: 'scale(0)',
+            opacity: 0,
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class AppBlhxWivesComponent implements OnInit {
   constructor(private _http: HttpClient) {}
+
+  @ViewChild('audio') audioEl!: ElementRef<HTMLMediaElement>;
+
   blhxData: BlhxData[] = [];
-  r: number = NaN;
+  randomIndex: number = NaN;
+  linesIndex: number = -1;
+  isShowLines = false;
+  linesEndTimer: NodeJS.Timeout | null = null;
   get currentData() {
     if (!this.blhxData.length) return null;
-    if (Number.isNaN(this.r)) {
-      this.r = Math.ceil((this.blhxData.length - 1) * Math.random());
-    }
-    return this.blhxData[this.r] ?? null;
+    const getRandomIndex = () =>
+      (this.randomIndex = Math.ceil(
+        (this.blhxData.length - 1) * Math.random()
+      ));
+    if (Number.isNaN(this.randomIndex)) getRandomIndex();
+    const rightData = () => {
+      if (this.blhxData[this.randomIndex].imageList.length) return;
+      getRandomIndex();
+      rightData();
+    };
+    rightData();
+    console.log(this.randomIndex);
+    return this.blhxData[this.randomIndex] ?? null;
   }
   ngOnInit(): void {
     this._init();
   }
   async _init() {
     const localData = localStorage.blhxWivesData;
-    if (localStorage) {
+    if (localStorage.blhxWivesData) {
       this.blhxData = JSON.parse(localData);
     } else {
       const res = await this._http
@@ -44,5 +86,24 @@ export class AppBlhxWivesComponent implements OnInit {
       this.blhxData = res;
       window.localStorage.blhxWivesData = JSON.stringify(res);
     }
+  }
+  onClick() {
+    if ((this.currentData?.lines.length ?? 0) <= 0) return;
+    if (this.linesIndex < this.currentData!.lines.length - 1)
+      this.linesIndex += 1;
+    else this.linesIndex = 0;
+    if (this.linesEndTimer) clearTimeout(this.linesEndTimer);
+    setTimeout(() => {
+      const el = this.audioEl.nativeElement;
+      const duration = Number.isNaN(el.duration) ? 5 : el.duration;
+      el.play().catch((error) => {
+        console.log('播放失败');
+        console.log(error);
+      });
+      this.isShowLines = true;
+      this.linesEndTimer = setTimeout(() => {
+        this.isShowLines = false;
+      }, Math.ceil(duration) * 1000);
+    }, 500);
   }
 }
