284a1e19e8e97c55dbbf6f4e952538ac6b2e0e19
yzj <2538340934@qq.com>
2022-02-11 10:57:46 +0800

    fix: 修复scss样式规则适配ng13

--- src/app/shared/services/xss.service.ts
+++ src/app/shared/services/xss.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { filterXSS, IFilterXSSOptions } from 'xss';
+// import { filterXSS, IFilterXSSOptions } from 'xss';
 
 @Injectable({
   providedIn: 'root',
 })
 // make love,no xss!
 export class XssService {
-  constructor() {}
-  filterXSS(html: string, options?: IFilterXSSOptions): string {
-    return filterXSS(html, options);
+  constructor() { }
+  filterXSS(html: string, options?: any): string {
+    return (window as any).filterXSS(html, options);
   }
 }
